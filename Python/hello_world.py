eb55eb94cb3b903d3712928eb0a4af8969229b4d
Brian McConchie <bmcconchie1@gmail.com>
2017-10-02 00:13:24 -0700

    updated 10/2/2017

---
+++ Python/hello_world.py
@@ -0,0 +1,7 @@
+fruits = ['apple','banna','orange','strawberry']
+vegetables = ['lettuce','cucumber', 'carrots']
+
+fruits_and_vegetables = fruits + vegetables
+print fruits_and_vegetables
+salad = 3 * vegetables
+print salad
